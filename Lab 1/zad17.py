db7f9b266bdb94d0cd109468c46cba2b96fde24f
dawidnovi <101060980+dawidnovi@users.noreply.github.com>
2022-03-28 15:08:38 +0200

    Add files via upload

---
+++ Lab 1/zad17.py
@@ -0,0 +1,7 @@
+a = 3
+if a % 2 == 0:
+    print("a jest parzysta!")
+else:
+    print("a nie jest parzysta")
+    if a % 3 == 0:
+        print("a jest podzielna przez 3")
---
+++ Lab 1/zad18.py
@@ -0,0 +1,8 @@
+a = 0
+
+if a < 0:
+    print("a jest liczbą ujemną")
+elif a > 0:
+    print("a jest liczbą dodatnią")
+else:
+    print("a równa się 0")
